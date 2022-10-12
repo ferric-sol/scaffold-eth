03587af111b3de2eefc6170961a9d6e324459b84
ferric <ferric-sol@ferric.net>
2022-10-11 22:49:40 -0400

    Modifiers Task 1

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,6 +5,10 @@ import "hardhat/console.sol";
 
 contract YourContract {
 
+  address public owner;
+
+  uint private constant Fee = 2;
+
   mapping(address => uint256) public balances;
 
   struct userDetailsStruct {
@@ -13,6 +17,34 @@ contract YourContract {
   }
 
   mapping(address => userDetailsStruct) public userDetails;
+
+  constructor() {
+        // Set the transaction sender as the owner of the contract.
+        owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not owner");
+    // Underscore is a special character only used inside
+    // a function modifier and it tells Solidity to
+    // execute the rest of the code.
+    _;
+  }
+
+  modifier onlyDepositor() {
+    require(balances[msg.sender] > 0, "Not a previous depositor");
+    _;
+  }
+
+  error AmountTooSmall();
+
+  modifier checkFee(uint256 _amount) {
+    if(Fee > _amount) {
+      revert AmountTooSmall();
+    }
+    _;
+  }
+
 
   function deposit(uint256 _amount) public {
       balances[msg.sender] = _amount;
@@ -31,4 +63,11 @@ contract YourContract {
     return userDetails[msg.sender];
   }
 
+  function withdraw(uint256 _amount) public onlyOwner {
+    balances[msg.sender] -= _amount;
+  }
+
+  function addFund(uint256 _amount) public onlyDepositor checkFee (_amount) { 
+    balances[msg.sender] += _amount;
+  }
 }
