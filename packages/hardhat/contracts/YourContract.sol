0fe86d6802ae13cbbf9121cb473a9d4baa536a07
ferric <ferric-sol@ferric.net>
2022-10-11 21:38:31 -0400

    Structs Task 1

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -7,12 +7,28 @@ contract YourContract {
 
   mapping(address => uint256) public balances;
 
+  struct userDetailsStruct {
+    string name;
+    uint256 age;
+  }
+
+  mapping(address => userDetailsStruct) public userDetails;
+
   function deposit(uint256 _amount) public {
       balances[msg.sender] = _amount;
   }
 
   function checkBalance() public view returns (uint256) {
     return balances[msg.sender];
+  }
+
+  function setUserDetails(string calldata name, uint256 age) public {
+    userDetails[msg.sender].name = name;
+    userDetails[msg.sender].age = age;
+  }
+
+  function getUserDetail() public view returns (userDetailsStruct memory) {
+    return userDetails[msg.sender];
   }
 
 }
