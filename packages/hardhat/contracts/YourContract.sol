133d4909ca65f853197e36e7ab29d94167c8b2f5
ferric <ferric-sol@ferric.net>
2022-10-19 00:52:36 -0400

    Make deposit accept ether

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -4,12 +4,22 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 
 contract YourContract {
+  // Payable address can receive Ether
+  address payable public owner;
+
+  // Payable constructor can receive Ether
+  constructor() payable {
+    owner = payable(msg.sender);
+  }
+
+  // Function to deposit Ether into this contract.
+  // Call this function along with some Ether.
+  // The balance of this contract will be automatically updated.
+  function deposit() public payable {
+    balances[msg.sender] += msg.value;
+  }
 
   mapping(address => uint256) public balances;
-
-  function deposit(uint256 _amount) public {
-      balances[msg.sender] = _amount;
-  }
 
   function checkBalance() public view returns (uint256) {
     return balances[msg.sender];
